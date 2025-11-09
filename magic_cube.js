2e1e59bc13ea612fdb14a4c8193d78c79a934950
Hanyu Shi <hanyushiy@gmail.com>
2025-11-09 15:23:08 +0100

    add .gitignore

--- magic_cube.js
+++ magic_cube.js
@@ -7,27 +7,6 @@ class Orientation {
     }
 }
 
-// new Orientation(1, 1, 2)
-//
-//
-// let orientation = {
-//     x: 0,
-//     y: 0,
-//     z: 0,
-// };
-//
-// Object.defineProperty(orientation, 'cubelet_face_orientation', {
-//     get() {
-//         return `(${this.x},${this.y},${this.z})`
-//     },
-//     set(value) {
-//         [this.x, this.y, this.z] = value.split(" ")
-//     }
-// });
-//
-// alert(orientation.cubelet_face_orientation);
-//
-// for (let key in orientation) alert(key);
 
 let a = new Orientation(1,2,4)
 
