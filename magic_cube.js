e60ac7653c1a9287f02874eedf68e28cc3f559bd
Hanyu Shi <hanyushiy@gmail.com>
2025-11-10 11:07:40 +0100

    add instances of Layer

--- magic_cube.js
+++ magic_cube.js
@@ -8,11 +8,11 @@ class Orientation {
 
 const Orientations = Object.freeze({
         FRONT_ORIENTATED: new Orientation(1, 0, 0),
-        BACK_ORIENTATED: Orientation(-1, 0, 0),
-        TOP_ORIENTATED: Orientation(0, 0, 1),
-        BOTTOM_ORIENTATED: Orientation(0, 0, -1),
-        LEFT_ORIENTATED: Orientation(0, -1, 0),
-        RIGHT_ORIENTATED: Orientation(0, 1, 0),
+        BACK_ORIENTATED: new Orientation(-1, 0, 0),
+        TOP_ORIENTATED: new Orientation(0, 0, 1),
+        BOTTOM_ORIENTATED: new Orientation(0, 0, -1),
+        LEFT_ORIENTATED: new Orientation(0, -1, 0),
+        RIGHT_ORIENTATED: new Orientation(0, 1, 0),
     }
 )
 
@@ -56,6 +56,27 @@ class Layer {
         this.contains = new Function(contains)
     }
 }
+
+const Layers = Object.freeze({
+    FRONT_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: 1, y: 0, z: 0}) return true
+    }),
+    BACK_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: -1, y: 0, z: 0}) return true
+    }),
+    TOP_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: 0, y: 0, z: 1}) return true
+    }),
+    BOTTOM_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: 0, y: 0, z: -1}) return true
+    }),
+    LEFT_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: 0, y: -1, z: 0}) return true
+    }),
+    RIGHT_LAYER: new Layer((cubelet) => {
+        if (cubelet.position === {x: 0, y: 1, z: 0}) return true
+    })
+})
 
 class ColoredCubelet {
     constructor(x, y, z) {
@@ -112,7 +133,7 @@ class Cube {
         ]
     }
 
-    // wip: 
+    // wip:
     // coloring() {
     //     this.cubelets.forEach((cubelet) =>{
     //         if ()
