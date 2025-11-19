4d4acc5837a1c92c24bccf91d55ded15ecd762f5
Hanyu Shi <hanyushiy@gmail.com>
2025-11-19 23:23:29 +0100

    fix: update ColoredCubelet to extend Cubelet

--- magic_cube.js
+++ magic_cube.js
@@ -87,28 +87,28 @@ class Layer {
 
 const Layers = Object.freeze({
     FRONT_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.x === 1,
+            colored_cubelet.position.x === 1,
         Orientations.X_AXIS),
     BACK_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.x === -1,
+            colored_cubelet.position.x === -1,
         Orientations.X_AXIS),
     TOP_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.z === 1,
+            colored_cubelet.position.z === 1,
         Orientations.Z_AXIS),
     BOTTOM_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.z === -1,
+            colored_cubelet.position.z === -1,
         Orientations.Z_AXIS),
     LEFT_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.y === -1,
+            colored_cubelet.position.y === -1,
         Orientations.Y_AXIS),
     RIGHT_LAYER: new Layer(colored_cubelet =>
-            colored_cubelet.cubelet.position.y === 1,
+            colored_cubelet.position.y === 1,
         Orientations.Y_AXIS)
 })
 
-class ColoredCubelet {
+class ColoredCubelet extends Cubelet {
     constructor(x, y, z) {
-        this.cubelet = new Cubelet(x, y, z)
+        super(x, y, z)
         this.faces = [new CubeletFace(Orientations.FRONT_ORIENTATED), new CubeletFace(Orientations.BACK_ORIENTATED), new CubeletFace(Orientations.TOP_ORIENTATED), new CubeletFace(Orientations.BOTTOM_ORIENTATED), new CubeletFace(Orientations.LEFT_ORIENTATED), new CubeletFace(Orientations.RIGHT_ORIENTATED)]
     }
 
@@ -135,7 +135,7 @@ class ColoredCubelet {
     }
 
     rotateXClockwise(angle) {
-        this.cubelet.rotateXClockwise(angle)
+        super.rotateXClockwise(angle)
         this.faces.forEach((face) => {
             face.rotateXClockwise(angle)
         })
@@ -204,8 +204,8 @@ class Cube {
         let front = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.FRONT_LAYER.contains(colored_cubelet)) {
-                const y = colored_cubelet.cubelet.position.y + 1
-                const z = colored_cubelet.cubelet.position.z + 1
+                const y = colored_cubelet.position.y + 1
+                const z = colored_cubelet.position.z + 1
 
                 front[y][z] = colored_cubelet.getFaceOn(Orientations.FRONT_ORIENTATED)
             }
@@ -217,8 +217,8 @@ class Cube {
         let back = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.BACK_LAYER.contains(colored_cubelet)) {
-                const y = colored_cubelet.cubelet.position.y + 1
-                const z = colored_cubelet.cubelet.position.z + 1
+                const y = colored_cubelet.position.y + 1
+                const z = colored_cubelet.position.z + 1
 
                 back[y][z] = colored_cubelet.getFaceOn(Orientations.BACK_ORIENTATED)
             }
@@ -230,8 +230,8 @@ class Cube {
         let left = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.LEFT_LAYER.contains(colored_cubelet)) {
-                const z = colored_cubelet.cubelet.position.z + 1
-                const x = colored_cubelet.cubelet.position.x + 1
+                const z = colored_cubelet.position.z + 1
+                const x = colored_cubelet.position.x + 1
 
                 left[z][x] = colored_cubelet.getFaceOn(Orientations.LEFT_ORIENTATED)
             }
@@ -243,8 +243,8 @@ class Cube {
         let right = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.RIGHT_LAYER.contains(colored_cubelet)) {
-                const z = colored_cubelet.cubelet.position.z + 1
-                const x = colored_cubelet.cubelet.position.x + 1
+                const z = colored_cubelet.position.z + 1
+                const x = colored_cubelet.position.x + 1
 
                 right[z][x] = colored_cubelet.getFaceOn(Orientations.RIGHT_ORIENTATED)
             }
@@ -256,8 +256,8 @@ class Cube {
         let top = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.TOP_LAYER.contains(colored_cubelet)) {
-                const y = colored_cubelet.cubelet.position.y + 1
-                const x = colored_cubelet.cubelet.position.x + 1
+                const y = colored_cubelet.position.y + 1
+                const x = colored_cubelet.position.x + 1
 
                 top[x][y] = colored_cubelet.getFaceOn(Orientations.TOP_ORIENTATED)
             }
@@ -269,8 +269,8 @@ class Cube {
         let bottom = Array.from({length: 3}, () => Array(3).fill(Colors.EMPTY))
         for (const colored_cubelet of this.cubelets) {
             if (Layers.BOTTOM_LAYER.contains(colored_cubelet)) {
-                const y = colored_cubelet.cubelet.position.y + 1
-                const x = colored_cubelet.cubelet.position.x + 1
+                const y = colored_cubelet.position.y + 1
+                const x = colored_cubelet.position.x + 1
 
                 bottom[x][y] = colored_cubelet.getFaceOn(Orientations.BOTTOM_ORIENTATED)
             }
@@ -294,11 +294,13 @@ class Cube {
 
 let a = new ColoredCubelet(1, 1, 0)
 let b = new Cube()
-b.rotateClockwise(Layers.FRONT_LAYER, Math.PI / 2, 1)
+b.rotateClockwise(Layers.FRONT_LAYER, Math.PI, 1)
 console.log(Layers.FRONT_LAYER.contains(a))
+
 b.getFront().print()
 b.getBack().print()
 b.getLeft().print()
 b.getRight().print()
 b.getTop().print()
 b.getBottom().print()
+
